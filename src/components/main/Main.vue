13538de19ec5134e2e48f9373b8398af508fbbe5
one4all <seodj93@naver.com>
2023-02-13 23:02:13 +0900

    20230213

---
+++ src/components/main/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="naverMap" id="map" style="width:100%;height:400px;"></div>
+</template>
+
+<script>
+export default {
+  name:'naverMap',
+  components:{
+  },
+  data(){
+    return{
+
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
